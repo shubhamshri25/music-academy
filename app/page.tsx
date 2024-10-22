d35af36ad965eada6dd334b271d1b23a56e3fb38
shubham <shubhshri25gmail.com>
2024-10-22 19:57:11 +0530

    moving cards done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 const Home = () => {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] ">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials/>
+      <Footer />
     </main>
   );
 };
